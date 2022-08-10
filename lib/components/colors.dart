e9c509a54bf8f46712549af114cbba9327f20f9d
220v-K <jwlee010222@gmail.com>
2022-08-10 22:14:52 +0900

    Feat: Main Page

--- lib/components/colors.dart
+++ lib/components/colors.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const main_color = Color.fromARGB(255, 149, 232, 172);
+const real_white = Color(0xffffffff);
+const real_black = Color.fromARGB(255, 25, 25, 25);
--- lib/main.dart
+++ lib/main.dart
@@ -12,20 +12,49 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'GmarketSans',
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: main_color,
-          title: const Text(
-            "HandySub",
-            style: TextStyle(fontFamily: 'GmarketSans'),
+      home: Builder(builder: (BuildContext context) {
+        final height = MediaQuery.of(context).size.height;
+        final width = MediaQuery.of(context).size.width;
+        return Scaffold(
+          body: Stack(
+            children: [
+              Container(
+                height: height,
+                width: width,
+                child: Image.asset(
+                  'assets/main_page_pic1.png',
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                alignment: Alignment(0.0, -0.5),
+                child: Image.asset('assets/main_page_logo.png'),
+              ),
+              Container(
+                alignment: Alignment(0.0, 0.75),
+                child: ElevatedButton(
+                  style: TextButton.styleFrom(
+                      backgroundColor: main_color,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30)),
+                      minimumSize: Size(width * 0.8, 45)),
+                  onPressed: () {
+                    // TODO: make Navigate to Login Page.
+                  },
+                  child: Text(
+                    "시작하기",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 20,
+                      color: real_black,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ),
-        body: Center(
-          child: Image.asset(
-            "assets/main_page_pic1.png",
-          ),
-        ),
-      ),
+        );
+      }),
     );
   }
 }
