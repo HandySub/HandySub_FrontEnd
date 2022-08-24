65321b493afbe5188d5d5a8e1058aa53e48b1704
jjunjji <younggy123@gmail.com>
2022-08-24 23:19:49 +0900

    HANDY-11 / Code Refactorying & fix error

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:handysub/components/frame.dart';
 import 'package:handysub/constants/colors.dart';
 import 'package:handysub/pages/Login/loginPage.dart';
+import 'package:handysub/pages/Login/signupPage.dart';
 import 'package:handysub/pages/Matching/matching_apply_disabled.dart';
 import 'package:handysub/pages/Matching/matching_list_disabled.dart';
 import 'package:handysub/pages/Matching/matching_pick_abled.dart';
@@ -53,7 +54,7 @@ class MyApp extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => MatchingPickAbled(),
+                        builder: (context) => SignupPage(),
                       ),
                     );
                   },
