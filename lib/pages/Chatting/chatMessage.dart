c264ecc28cf15ffbd3a3285648a804e461986699
220v-K <jwlee010222@gmail.com>
2022-08-30 23:48:57 +0900

    Edit Chat Bubble

--- lib/pages/Chatting/chatMessage.dart
+++ lib/pages/Chatting/chatMessage.dart
@@ -10,34 +10,33 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return userId==0? myMessage(context, text) : opponentMessage(context, text);
+    return userId == 0
+        ? myMessage(context, text)
+        : opponentMessage(context, text);
   }
 }
 
-Widget myMessage(BuildContext context, String text){
+Widget myMessage(BuildContext context, String text) {
   final width = MediaQuery.of(context).size.width;
   return Row(
     mainAxisAlignment: MainAxisAlignment.end,
     children: [
       Container(
-        padding: const EdgeInsets.fromLTRB(20,8, 20, 8),
         margin: const EdgeInsets.fromLTRB(5, 6, 5, 6),
-        width: width*0.52,
+        padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
+        constraints: BoxConstraints(maxWidth: width * 0.52),
         decoration: BoxDecoration(
             color: main_color_light,
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(15),
-              topRight:  Radius.circular(15),
-              bottomLeft:  Radius.circular(15),
-              bottomRight:  Radius.circular(0),
-            )
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-              color: real_black_65,
-            ),
+              topRight: Radius.circular(15),
+              bottomLeft: Radius.circular(15),
+              bottomRight: Radius.circular(0),
+            )),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: real_black_65,
           ),
         ),
       ),
@@ -45,30 +44,27 @@ Widget myMessage(BuildContext context, String text){
   );
 }
 
-Widget opponentMessage(BuildContext context, String text){
+Widget opponentMessage(BuildContext context, String text) {
   final width = MediaQuery.of(context).size.width;
   return Row(
     mainAxisAlignment: MainAxisAlignment.start,
     children: [
       Container(
-        padding: const EdgeInsets.fromLTRB(20,8, 20, 8),
         margin: const EdgeInsets.fromLTRB(5, 6, 5, 6),
-        width: width*0.52,
+        padding: const EdgeInsets.fromLTRB(20, 8, 20, 8),
+        constraints: BoxConstraints(maxWidth: width * 0.52),
         decoration: BoxDecoration(
             color: emerald,
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(0),
-              topRight:  Radius.circular(15),
-              bottomLeft:  Radius.circular(15),
-              bottomRight:  Radius.circular(15),
-            )
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-              color: real_white,
-            ),
+              topRight: Radius.circular(15),
+              bottomLeft: Radius.circular(15),
+              bottomRight: Radius.circular(15),
+            )),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: real_white,
           ),
         ),
       ),
@@ -76,8 +72,7 @@ Widget opponentMessage(BuildContext context, String text){
   );
 }
 
-
-Widget messageDate(String date){
+Widget messageDate(String date) {
   return Container(
     margin: const EdgeInsets.fromLTRB(5, 6, 5, 6),
     child: Center(
